ecbfd63705b7ddefd0d651e77082613638dc7b36
leonid.negaschev <leonid.negaschev@gmail.com>
2022-06-27 10:52:12 +0500

    Add settings for static files

--- smartsite/settings.py
+++ smartsite/settings.py
@@ -86,7 +86,9 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
