14953bd8a84ce46073ff5cf7db4666b437e332e3
leonid.negaschev <leonid.negaschev@gmail.com>
2022-06-29 17:03:58 +0500

    Add verbose_name

--- researchers/apps.py
+++ researchers/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ResearchersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'researchers'
+    verbose_name = 'Мировые исследователи'
