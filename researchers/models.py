18e2748190643238b48d1b03f4b1d474ff51ff7c
leonid.negaschev <leonid.negaschev@gmail.com>
2022-06-29 17:05:20 +0500

    Add nested class

--- researchers/models.py
+++ researchers/models.py
@@ -17,6 +17,11 @@ class Researchers(models.Model):
     def get_absolute_url(self):
         return reverse("post", kwargs={"post_id": self.pk})
 
+    class Meta:
+        verbose_name = 'Известные исследователи'
+        verbose_name_plural = 'Известные исследователи'
+        ordering = ['-time_create', 'title']
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)
