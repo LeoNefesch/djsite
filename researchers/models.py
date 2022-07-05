dd5068d30a2f0083e6d27658e0c9cae03cfc2beb
leonid.negaschev <leonid.negaschev@gmail.com>
2022-07-05 12:37:14 +0500

    Add slug field

--- researchers/models.py
+++ researchers/models.py
@@ -4,14 +4,13 @@ from django.urls import reverse
 
 class Researchers(models.Model):
     title = models.CharField(max_length=255, verbose_name="Заголовок")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
     content = models.TextField(blank=True, verbose_name="Текст статьи")
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
     time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
     is_published = models.BooleanField(default=True, verbose_name="Опубликовано")
-    cat = models.ForeignKey(
-        'Category', on_delete=models.PROTECT, null=True, verbose_name="Категории"
-    )
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name="Категории")
 
     def __str__(self):
         return self.title
@@ -27,6 +26,7 @@ class Researchers(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
 
     def __str__(self):
         return self.name
