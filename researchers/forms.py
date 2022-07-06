b64c000f0bdd01020dfb87dc5da8fd74852ef173
leonid.negaschev <leonid.negaschev@gmail.com>
2022-07-06 15:19:08 +0500

    Add displaying russian titles

--- researchers/forms.py
+++ researchers/forms.py
@@ -4,8 +4,10 @@ from .models import *
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
-    is_published = forms.BooleanField()
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(max_length=255, label="Заголовок")
+    slug = forms.SlugField(max_length=255, label="URL")
+    content = forms.CharField(
+        widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), label="Контент"
+    )
+    is_published = forms.BooleanField(label="Опубликовано")
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label="Категории")
