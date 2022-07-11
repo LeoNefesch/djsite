b900e4860d600d148a372d45116234647ab53f57
leonid.negaschev <leonid.negaschev@gmail.com>
2022-07-11 17:39:55 +0500

    Add compressed query for optimization

--- researchers/views.py
+++ researchers/views.py
@@ -23,7 +23,7 @@ class ResearchersHome(DataMixin, ListView):
         return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
-        return Researchers.objects.filter(is_published=True)
+        return Researchers.objects.filter(is_published=True).select_related('cat')
 
 
 """ def index(request):
@@ -116,13 +116,16 @@ class ResearchersCategory(DataMixin, ListView):
     allow_empty = False
 
     def get_queryset(self):
-        return Researchers.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
+        return Researchers.objects.filter(
+            cat__slug=self.kwargs['cat_slug'], is_published=True
+        ).select_related('cat')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        c = Category.objects.get(slug=self.kwargs['cat_slug'])
         c_def = self.get_user_context(
-            title='Категория - ' + str(context['posts'][0].cat),
-            cat_selected=context['posts'][0].cat_id,
+            title='Категория - ' + str(c.name),
+            cat_selected=c.pk,
         )
         return dict(list(context.items()) + list(c_def.items()))
 
