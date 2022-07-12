bec6f21edb3d2e63e6e6a924d8dd599bb8c0c2bd
leonid.negaschev <leonid.negaschev@gmail.com>
2022-07-12 11:33:17 +0500

    Change title and header of admin page; add displaying photo instead url photo

--- researchers/admin.py
+++ researchers/admin.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import *
 
 
 class ResearchersAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+    list_display = ('id', 'title', 'time_create', 'get_html_photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_create')
     prepopulated_fields = {"slug": ('title',)}
+
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f"<img src='{object.photo.url}' width=50>")
+
+    get_html_photo.short_description = 'Миниатюра'
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -21,3 +28,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Researchers, ResearchersAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+
+admin.site.site_title = 'Админ-панель сайта об учёных'
+admin.site.site_header = 'Админ-панель сайта об учёных 2'
